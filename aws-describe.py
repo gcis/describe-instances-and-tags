b52288271da8c5db4874271c3e694cfd4e940e1c
gabriel <gabriel@gcismondi.com>
2016-06-17 20:16:07 +0200

    added arguments and created tag_filter for describe-intances

--- aws-describe.py
+++ aws-describe.py
@@ -1,25 +1,38 @@
 import boto3
-import sys, getopt
+import sys, argparse
+import json
 
-def main(argv=None):
-   inputfile = ''
-   outputfile = ''
-   try:
-      opts, args = getopt.getopt(argv,"divt:o:",["describe-tags","ip-address","verbose","tag=","ofile="])
-   except getopt.GetoptError:
-      print 'test.py -i <inputfile> -o <outputfile>'
-      sys.exit(2)
-   for opt, arg in opts:
-      if opt in ("-d", "--describe-tags"):
-         print 'describe tags'
-         sys.exit()
-      elif opt in ("-i", "--ifile"):
-         inputfile = arg
-      elif opt in ("-o", "--ofile"):
-         outputfile = arg
-   print 'Input file is "', inputfile
-   print 'Output file is "', outputfile
 
-if __name__ == "__main__":
-   sys.exit(main())
-   
+parser = argparse.ArgumentParser(description='Describe AWS Ec2 resources')
+parser.add_argument('-d', '--describe-tags', action='store_true', help='describe tags parameter', default=False)
+parser.add_argument('-tn', '--tags', action='store', nargs='+', help='tag or tags to descibe', default=[])
+parser.add_argument('-gip', '--get-ip-address', action='store_true', help='returns just ip addresses', default=False)
+parser.add_argument('-gn', '--get-instance-name', action='store_true', help='returns just ip addresses', default=False)
+parser.add_argument('-gid', '--get-instance-id', action='store_true', help='returns just ip addresses', default=False)
+parser.add_argument('-gsg', '--get-security-group', action='store_true', help='returns just ip addresses', default=False)
+parser.add_argument('-v', '--verbose', action='store_true', help='returns just ip addresses', default=False)
+
+
+args = parser.parse_args()
+print args
+
+#Lets check tags and build the array for the describe request
+tag_filters = []
+def simplify_tags(tagname) :
+   if tagname == 'asg' :
+      return 'tag:aws:autoscaling:groupName'
+   else :
+      return tagname
+
+for i in range(len(args.tags)) :
+   splitted = args.tags[i].split('=')
+   if len(splitted) == 2 :
+      data = {}
+      data['Name'] = simplify_tags(splitted[0])
+      data['Values'] = splitted[1]
+      json_data = json.dumps(data)
+      tag_filters.append(json_data) 
+   else :
+      print 'Tags were not written in the right way, You must specify tag_name=tag_value'
+
+print tag_filters
