8888ccc688c75e8476889fe1667bcc125ce694cd
gabriel <gabriel@gcismondi.com>
2016-06-17 20:45:12 +0200

    removed hard coded credntials

--- aws-describe.py
+++ aws-describe.py
@@ -5,13 +5,13 @@ import json
 
 parser = argparse.ArgumentParser(description='Describe AWS Ec2 resources')
 parser.add_argument('-d', '--describe-tags', action='store_true', help='describe tags parameter', default=False)
-parser.add_argument('-tn', '--tags', action='store', nargs='+', help='tag or tags to descibe', default=[])
-parser.add_argument('-gip', '--get-ip-address', action='store_true', help='returns just ip addresses', default=False)
-parser.add_argument('-gn', '--get-instance-name', action='store_true', help='returns just ip addresses', default=False)
-parser.add_argument('-gid', '--get-instance-id', action='store_true', help='returns just ip addresses', default=False)
-parser.add_argument('-gsg', '--get-security-group', action='store_true', help='returns just ip addresses', default=False)
+parser.add_argument('-t', '--tags', action='store', nargs='+', help='tag or tags to descibe', default=[])
+parser.add_argument('-ip', '--get-ip-address', action='store_true', help='returns just ip addresses', default=False)
+parser.add_argument('-n', '--get-instance-name', action='store_true', help='returns just ip addresses', default=False)
+parser.add_argument('-id', '--get-instance-id', action='store_true', help='returns just ip addresses', default=False)
+parser.add_argument('-sg', '--get-security-group', action='store_true', help='returns just ip addresses', default=False)
 parser.add_argument('-v', '--verbose', action='store_true', help='returns just ip addresses', default=False)
-
+parser.add_argument('-r', '--region', action='store_true', help='region', default=False)
 
 args = parser.parse_args()
 print args
@@ -36,3 +36,37 @@ for i in range(len(args.tags)) :
       print 'Tags were not written in the right way, You must specify tag_name=tag_value'
 
 print tag_filters
+
+client = boto3.client(
+   'ec2',
+   region_name=args.region_name
+)
+
+if args.describe_tags :
+   response = client.describe_tags()
+   print response
+else :
+   response = client.describe_instances(
+      Filters = tag_filters
+   )
+
+
+# inputfile = ''
+# outputfile = ''
+# try:
+#    opts, args = getopt.getopt(argv,"divt:o:",["describe-tags","ip-address","verbose","tag=","ofile="])
+# except getopt.GetoptError:
+#    print 'test.py -i <inputfile> -o <outputfile>'
+#    sys.exit(2)
+# for opt, arg in opts:
+#    if opt in ("-d", "--describe-tags"):
+#       print 'describe tags'
+#       sys.exit()
+#    elif opt in ("-i", "--ifile"):
+#       inputfile = arg
+#    elif opt in ("-o", "--ofile"):
+#       outputfile = arg
+# print 'Input file is "', inputfile
+# print 'Output file is "', outputfile
+
+
