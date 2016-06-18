50783195201fdec3c921721301ed3d8316a8d90a
gabriel <gabriel@gcismondi.com>
2016-06-18 09:12:30 +0200

    added describe tags

--- aws-describe.py
+++ aws-describe.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import boto3
 import sys, argparse
 import json
@@ -14,7 +15,6 @@ parser.add_argument('-v', '--verbose', action='store_true', help='returns just i
 parser.add_argument('-r', '--region', action='store', help='region', default=None)
 
 args = parser.parse_args()
-print args
 
 #Lets check tags and build the array for the describe request
 tag_filters = []
@@ -22,20 +22,20 @@ def simplify_tags(tagname) :
    if tagname == 'asg' :
       return 'tag:aws:autoscaling:groupName'
    else :
-      return tagname
+      return 'tag:' + tagname
 
 for i in range(len(args.tags)) :
    splitted = args.tags[i].split('=')
    if len(splitted) == 2 :
       data = {}
       data['Name'] = simplify_tags(splitted[0])
-      data['Values'] = splitted[1]
-      json_data = json.dumps(data)
-      tag_filters.append(json_data) 
+      values = splitted[1].split(',')
+      data['Values'] = []
+      for j in range(len(values)) :
+         data['Values'].append(values[j])
+      tag_filters.insert(len(tag_filters), data) 
    else :
-      print 'Tags were not written in the right way, You must specify tag_name=tag_value'
-
-print tag_filters
+      print('Tags were not written in the right way, You must specify tag_name=tag_value')
 
 #setting the region is specified or using the default config if not
 if args.region is not None :
@@ -48,12 +48,23 @@ else :
 
 #making the request
 if args.describe_tags :
+   print('')
+   av_tags = []
    response = client.describe_tags()
-   print response
+   for i in range(len(response['Tags'])) :
+      if response['Tags'][i]['Key'] not in av_tags :
+         av_tags.append(response['Tags'][i]['Key'])
+         print(response['Tags'][i]['Key'], '-->', end=' ')
+         for j in range(len(response['Tags'])) :
+            if response['Tags'][j]['Key'] == response['Tags'][i]['Key'] : 
+               print(response['Tags'][j]['Value'], end=' | ')
+         print('')
+         print('-------------------------------------------------------------')
 else :
    response = client.describe_instances(
       Filters = tag_filters
    )
+   print(response)
 
 
 
