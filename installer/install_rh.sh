885455e64a41b0669d6c844a01fd897c82408f2c
Gabriel Cismondi <gabrielcismondi@gmail.com>
2016-06-20 12:28:13 +0200

    modified installer

--- installer/install_rh.sh
+++ installer/install_rh.sh
@@ -1,6 +1,6 @@
 sudo yum install git -y
 git clone git://github.com/ansible/ansible.git --recursive
-cd ./ansible
+cd ansible
 sudo make
-sudo sudo make install
+sudo make install
 ansible-playbook -i "localhost," -c local ../playbook.yml
